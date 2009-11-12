86716d15181e27f78d71eb9677df64de45e87302
mh <mh@immerda.ch>
2009-11-12 11:18:33 +0100

    fix restart and kill command to work properly

--- manifests/openbsd.pp
+++ manifests/openbsd.pp
@@ -1,7 +1,7 @@
 class ntp::openbsd inherits ntp::base {
     Service[ntpd]{
-        restart => "kill -HUP `ps ax | grep ntpd | head -n 1 | awk '{ print $1 }'`",
-        stop => "kill `ps ax | grep ntpd | head -n 1 | awk '{ print $1 }'`",
+        restart => "kill -HUP `ps ax | grep -v grep | grep ntpd | head -n 1 | awk '{ print \$1 }'`",
+        stop => "kill `ps ax | grep -v grep | grep ntpd | head -n 1 | awk '{ print \$1 }'`",
         start => '/usr/sbin/ntpd',
         hasstatus => false,
     }
