b21cb528f573b709fedbb76c4a0260d2cc60cae8
mh <mh@immerda.ch>
2010-10-17 00:49:31 +0200

    proper fix for that problem

--- manifests/xenu.pp
+++ manifests/xenu.pp
@@ -1,15 +1,7 @@
 # on domU's we make the clock independent
 class ntp::xenu {
-  case $operatingsystem {
-    debian,ubuntu: {
-      exec{'echo "jiffies"> /sys/devices/system/clocksource/clocksource0/current_clocksource':
-        unless => 'grep -q jiffies /sys/devices/system/clocksource/clocksource0/current_clocksource',
-      }
-    }
-    default: {
-      exec{'echo 1 > /proc/sys/xen/independent_wallclock':
-        unless => 'grep -q 1 /proc/sys/xen/independent_wallclock',
-      }
-    }
+  exec{'echo 1 > /proc/sys/xen/independent_wallclock':
+    unless => 'grep -q 1 /proc/sys/xen/independent_wallclock',
+    onlyif => 'test -f /proc/sys/xen/independent_wallclock',
   }
 }
