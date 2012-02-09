980b1a5abd972cf4892079fadd65f1a9bd774874
mh <mh@immerda.ch>
2012-02-09 16:18:47 +0100

    qualify that bin as sometimes this gave problems

--- manifests/xenu.pp
+++ manifests/xenu.pp
@@ -2,6 +2,6 @@
 class ntp::xenu {
   exec{'echo 1 > /proc/sys/xen/independent_wallclock':
     unless => 'grep -q 1 /proc/sys/xen/independent_wallclock',
-    onlyif => 'test -f /proc/sys/xen/independent_wallclock',
+    onlyif => '/usr/bin/test -f /proc/sys/xen/independent_wallclock',
   }
 }
