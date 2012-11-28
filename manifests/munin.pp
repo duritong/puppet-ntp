f5b098c95b425ab44db93d4641a33e7f7242f831
mh <mh@immerda.ch>
2012-11-28 18:55:23 +0100

    linting

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,5 +1,6 @@
+# configure monitoring of the confiured ntp servers
 class ntp::munin {
-  $ntps = regsubst(reject(split($::configured_ntp_servers, " "), '127\.127\.1\.0'),"(.+)", "ntp_\\1")
+  $ntps = regsubst(reject(split($::configured_ntp_servers, ' '), '127\.127\.1\.0'),'(.+)', "ntp_\\1")
 
   munin::plugin::deploy{$ntps:
     source => 'ntp/munin/ntp_',
