cfc48bb9b2a8c5cfa121fdb10c0da9c65daff734
mh <mh@immerda.ch>
2013-02-28 20:25:28 +0100

    also manage the seltype

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -3,6 +3,7 @@ class ntp::munin {
   $ntps = regsubst(reject(split($::configured_ntp_servers, ' '), '127\.127\.1\.0'),'(.+)', "ntp_\\1")
 
   munin::plugin::deploy{$ntps:
-    source => 'ntp/munin/ntp_',
+    source  => 'ntp/munin/ntp_',
+    seltype => 'munin_services_plugin_exec_t',
   }
 }
